b180c0a2163e125add7cbd16dbd44857c5fe4d61
CosmoumaDD <cosmo4267@gmail.com>
2025-04-25 16:22:42 +0800

    Update SelectImageDetectPage.java

--- VISTA_AndroidApp/app/src/main/java/com/example/vista/SelectImageDetectPage.java
+++ VISTA_AndroidApp/app/src/main/java/com/example/vista/SelectImageDetectPage.java
@@ -36,6 +36,8 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import java.io.IOException;
 
+import com.example.vista.TextToSpeech.CustomTextToSpeech;
+
 public class SelectImageDetectPage extends AppCompatActivity {
 
     private static final String TAG = "SelectImageDetectPage_debug";
@@ -44,6 +46,7 @@ public class SelectImageDetectPage extends AppCompatActivity {
     TextView txtResult;
 
     private OkHttpClient httpClient;
+    private CustomTextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +69,9 @@ public class SelectImageDetectPage extends AppCompatActivity {
                 .readTimeout(300, TimeUnit.SECONDS)
                 .writeTimeout(300, TimeUnit.SECONDS)
                 .build();
+
+        // Initialize CustomTextToSpeech
+        tts = new CustomTextToSpeech(this);
 
         // Get image URI from Intent
         Intent intent = getIntent();
@@ -179,6 +185,7 @@ public class SelectImageDetectPage extends AppCompatActivity {
                             // Update the TextView with the result
                             runOnUiThread(() -> {
                                 txtResult.setText(content);
+                                tts.speak(content);
                             });
                         } else {
                             Log.e(TAG, "postImageToServer: 'message' object is null");
